c683f0d89f79a8d5c68952d1b6ead497372d3ad8
yangz <yangzhennupt@163.com>
2019-05-04 18:02:22 +0800

    调整数据结构与算法目录

--- dataStructure/LinkedList.js
+++ dataStructure/LinkedList.js
@@ -1,5 +1,10 @@
+/**
+ * 
+ * 链表相比数组的优点：更改指针即可实现增删改，效率比数组依次移位更高！ 
+ */
+
 // 链表的节点
-function Node (element) {
+function Node(element) {
   this.element = element
   this.next = null
 }
@@ -7,19 +12,15 @@ function Node (element) {
 // 链表类
 
 function LinkedList() {
-  let head = null 
+  let head = null
   let length = 0
-  let Node = function (element) {
-    this.element = element
-    this.next = null
-  }
   // 链表尾部增加node
   this.append = function (element) {
     let node = new Node(element), current
-    if(head === null) {
+    if (head === null) {
       head = node
     } else {
-      current =  head
+      current = head
       while (current.next) {
         current = current.next
       }
@@ -32,9 +33,9 @@ function LinkedList() {
   }
   this.removeAt = function (position) {
     let current = head,
-    previous,
-    index= 0
-    if(position === 0) {
+      previous,
+      index = 0
+    if (position === 0) {
       head = current.next
     } else {
       while (index++ < position) {
@@ -46,9 +47,9 @@ function LinkedList() {
     length--;
     return current.element
   }
-  this.remove  = function (element) {
+  this.remove = function (element) {
     let previous, current = head
-    if(element === head.element) {
+    if (element === head.element) {
       head = current.next
     } else {
       while (current.element !== element) {
@@ -58,6 +59,6 @@ function LinkedList() {
       previous.next = current.next
     }
     length--;
-    
+
   }
 }
